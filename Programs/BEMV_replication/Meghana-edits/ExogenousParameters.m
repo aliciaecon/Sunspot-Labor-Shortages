5d5ac90720e14c8a3e726a26edce1c927ec2e0da
Meghana Gaur <gaur.meghana@gmail.com>
2022-07-12 16:38:35 -0400

    fix shape of disutility function

--- Programs/BEMV_replication/Meghana-edits/ExogenousParameters.m
+++ Programs/BEMV_replication/Meghana-edits/ExogenousParameters.m
@@ -13,11 +13,11 @@ ExogParams.d     = 0.0002 ;        % Exogenous firm exit rate
 
 ExogParams.chi         = 1 ;       % Exogenous disutility
 ExogParams.nbar        = 1000 ;    % Upper bound of truncated normal 
-ExogParams.mu_under    = 50;       % Mean of truncated normal 
+ExogParams.mu_under    = 0;       % Mean of truncated normal 
 ExogParams.sigma_under = 10 ;      % St dev of truncated normal 
 
 % check the functions to make sure they make sense
-xgrid=linspace(0,100,101);
+xgrid=linspace(0,nbar,250);
 for i=1:size(xgrid,2)
     [x(i), y(i)] = understaff(xgrid(i), ExogParams.mu_under, ExogParams.sigma_under, ExogParams.nbar);
 end
